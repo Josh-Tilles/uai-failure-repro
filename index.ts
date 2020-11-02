00b577e053a2b83d20713877e2e991f10bd7c73e
Josh Tilles <MerelyAPseudonym@gmail.com>
2020-11-01 22:11:45 -0500

    Create demo user-assigned identity

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-nextgen/resources/v20200601";
+import { UserAssignedIdentity } from "@pulumi/azure-nextgen/managedidentity/v20181130";
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup", {
     resourceGroupName: "josh-repro",
     location: "eastus2",
+});
+
+const exampleIdentity = new UserAssignedIdentity('example', {
+    location: "eastus2",
+    resourceGroupName: resourceGroup.name,
+    resourceName: 'example'
 });
