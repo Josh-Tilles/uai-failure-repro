b370e9762c84ccf830f8ccdac90085b469b0258e
Josh Tilles <MerelyAPseudonym@gmail.com>
2020-11-01 22:05:40 -0500

    Pin resource group to stable version

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-nextgen/resources/latest";
+import * as resources from "@pulumi/azure-nextgen/resources/v20200601";
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup", {
