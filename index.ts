b9179b3abf6f460aa8de3836d786ebbf3e6b29b6
Josh Tilles <MerelyAPseudonym@gmail.com>
2020-11-01 22:07:34 -0500

    Tweak RG params for my convenience

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import * as resources from "@pulumi/azure-nextgen/resources/v20200601";
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup", {
-    resourceGroupName: "my-rg",
-    location: "westus",
+    resourceGroupName: "josh-repro",
+    location: "eastus2",
 });
